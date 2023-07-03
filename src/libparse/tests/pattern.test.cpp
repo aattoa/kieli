2ea9ad220135504a3dea322908f0ed1f65c87505
aattoa <aattoa@proton.me>
2023-07-03 15:56:37 +0300

    libformat implementation
    - Implemented expression, pattern, and type cst formatting
    - libparse tests now all pass

--- src/libparse/tests/pattern.test.cpp
+++ src/libparse/tests/pattern.test.cpp
@@ -10,9 +10,9 @@ namespace { constexpr auto parse = libparse::test_parse_pattern; }
 
 TEST("literals") {
     REQUIRE_SIMPLE_PARSE("5");
-    REQUIRE(parse("5e3") == "5000");
+    REQUIRE_SIMPLE_PARSE("5e3");
     REQUIRE_SIMPLE_PARSE("5.0");
-    REQUIRE(parse("5.0e3") == "5300.0");
+    REQUIRE_SIMPLE_PARSE("5.0e3");
     REQUIRE_SIMPLE_PARSE("true");
     REQUIRE_SIMPLE_PARSE("false");
     REQUIRE_SIMPLE_PARSE("'x'");
