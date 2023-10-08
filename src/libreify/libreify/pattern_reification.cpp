36fa19a36aede266db084d261daf610ff19674ad
aattoa <aattoa@proton.me>
2023-10-08 14:14:46 +0300

    Remove libreify

--- src/libreify/libreify/pattern_reification.cpp
+++
@@ -1,9 +0,0 @@
-#include <libutl/common/utilities.hpp>
-#include <libreify/reification_internals.hpp>
-
-using namespace libreify;
-
-auto libreify::Context::reify_pattern(hir::Pattern const&) -> cir::Pattern
-{
-    utl::todo();
-}
